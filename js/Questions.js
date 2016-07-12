b901709ed7149983ce62ef71c5fb569d32a8fe74
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-07-12 09:43:52 -0400

    latest

--- js/Questions.js
+++ js/Questions.js
@@ -1,81 +1,48 @@
 
 $(document).ready(function(){
 
-	var randomNumber;
-	var clicks=0; 
+	var question1 = {
+	    pic: "img/gspic1.jpg",
+	    ask: "What is the alternative name for the German Shepherd?"
+	    choices: ["K9", "Alsatian", "Rommel", "Rin Tin Tin"],
+	    correntans: ["Alsatian"],
+	    info: "During World Wars I and II German Shepherds were often used in the war effort .This extremely intelligent breed served as messenger dogs, guard dogs. and search and 1escue animals.\
+	    	Fearing the anti-German sentiments prevalent in the U\.S.and England during World War I would spread to the dogs, officials began calling them Alsatians.  It was not until 1930 that the name German Shepherd was reestablished in England."
+	    }
 
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	var question2 = {
+	    pic: "img/gspic2.jpg",
+	    ask: "German Shepherds are often used as _______ dogs."
+	    choices: ["Purse", "Show", "Working", "Lounging"],
+	    correntans: ["Working"],
+	    info: "German Shepherds are working dogs. Highly trainable, loyal and obedient they have been used in various capacities. As their name implies they have often been used in herding, but also serve as search and rescue dogs, guide dogs, and police dogs. If not trained properly though, these strong dogs can be quite aggressive. They have sometimes been subject to Breed Specific Legislation, which are laws controlling or even prohibiting certain breeds, including German Shepherds, in certain areas."
+    	}
 
-  	});
+    	var question3 = {
+	    pic: "img/gspic3.jpg",
+	    ask: "According to the Amrican Kennel Club's list of most popu;ar dogs, the German Shepherd ranks in the top ten."
+	    choices: ["True", "False"],
+	    correntans: ["True"],
+	    info: "They are the fourth most popular dog in the U.S. with over 45,000 registered dogs. They are, in fact, well known and beloved throughout the world, ranking near the top of most popular dogs in several countries."
+	    }
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	var question4 = {
+	    pic: "img/gspic4.jpg",
+	    ask: "Which U.S. President owned a German Shepherd?"
+	    choices: ["Bill Clinton", "Richard Nixon", "Lyndon B. Johnson", "Franklin D. Roosevelt"],
+	    correntans: ["Franklin D. Roosevelt"],
+	    info: "Roosevelt owned a German Shepherd named Major. FDR was a great dog lover and had several pets throughout his life and presidential terms. Major does not seem to have been the most disciplined of dogs though. He once bit the visiting British Prime Minister Ramsey McDonald! Thankfully, no international crisis between the U.S. and Great Britain ensued."
 
-	$(".new").click(function(){
-		location.reload();
-	});
+    	}var question5 = {
+	    pic: "img/gspic5.jpg",
+	    ask: "To what or whom does the name 'Schutzhund?' refer?"
+	    choices: ["The father of the German Shepherd breed", "A test to determine the worthiness of a German Shepherd", "The Kentucky Derby of dog racing", "The original dog kennel where German Shepherds were bred"],
+	    correntans: ["A test to determine the worthiness of a German Shepherd"],
+	    info: "The Schutzhund was begun in Germany at the beginning of the 19th Century to evaluate the character of German Shepherds. As the show ringjudges appearance to see if the dog meets certain established standards, the Schutzhund judges behavior, hoping to determine if dogs have the qualities of a true German Shepherd. If the dog passes the test they are capable of a variety of tasks, including police work, odor detection, search and rescue, as well as others."
+	    }
 
-  	$("#guessButton").click(function(e) {
-
-  		e.preventDefault();
- 
-  		if (randomNumber == null) {
-  			randomNumber = generateNumber() 
-  		};
-
-  		clickCounter();
-  		compareNumbers();
-
-  		function clickCounter() {							
-  			$("#count").html(clicks += 1);
-  		};
-  	
-  	 	function generateNumber() {	
-  			var minNumber = 1;
-  			var maxNumber = 100;
-  			var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-  				
-  			return randomNumber;
-  	 	};
-
-	  	function compareNumbers () {
-	  		var userGuess = $("#userGuess").val();   
-  	 		var difference = Math.abs(parseInt(userGuess - randomNumber));
-
-  	 		$("#guessList").append("<li>" + userGuess + "</li>");
-
-	  		function wrongNumber(message) {   
-	  			confirm(message);
-	  		};
-
-
-			if (difference == 0) {
-				alert ("You Guessed Correctly! Hooray!");
-
-			} else if (difference >= 50 && difference <= 100) {
-				wrongNumber("You are ice cold! Try again!");
-
-			} else if (difference >= 30 && difference <= 49) {
-				wrongNumber("You are cold! Try again!");
-
-			} else if (difference >= 20 && difference <= 29)  {
-				wrongNumber("You are warm! Try again!");
-
-			} else if (difference >= 10 && difference <= 19)  {
-				wrongNumber("You are hot! Try again!");
-
-			} else if (difference >= 1 && difference <= 9)  {
-				wrongNumber("You are very hot! Try again!");
-
-			} else {
-				wrongNumber("You did not enter a number between 1 and 100.  Please try again!");
-			} 
-		};
-	});
+	
+  };
 });
 
 
