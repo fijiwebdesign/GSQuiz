2e087fa01714062b3e0c746c8f0a0736bed49c81
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-07-20 14:01:46 -0400

    latest

--- js/Application.js
+++ js/Application.js
@@ -1,30 +1,98 @@
 
 $(document).ready(function loaded (){
 
+	// Intro class shown; img with instructions to click. Other classes hidden
+
 	$("#intro").fadeIn(2000);
 	$("#play_area").hide();
 	$("#done").hide();
 
+	// User clicks on img, fades out intro class, fades in first ques via play area class
+
 	$("#introgspic").click(function introduction (e){
 		e.preventDefault(); // prevent the click from happening. 
-		$("#play_area").fadeIn(); // show play area
 		$("#intro").fadeOut(); // hide the intro
+		$("#play_area").fadeIn(); // show play area
+		$(".answer_area").hide();
 	});
 
-	questionList.forEach(function presentquestion (currentQues) { // each question in questionList Array
+	//Play area
+
+	//Ask user a question
 	
-	var possibleAnswers = currentQues.choices; // gabe: Array of choices for current question. Alternative syntax is: currentQues['choices']
+	var totalQuestions = questionList.length;
+	var currentQues = questionList[0];
+	var displayTheQues = $("#questions").append('<li class="ask">' + currentQues.ask + '</li>');
+	var displayAnsChoices = currentQues.choices.forEach(function getEachAnsChoice (ansChoice) {
+			$("#questions").append('<input id="choices" type="radio">' + ansChoice + '<br>')
+			});
+
+	displayTheQues;
+	displayAnsChoices;
+
+	//User Submits Answer
+
+	var uponSubmit = $(#subbtn).click(function checkAns (){
+			$("#play_area").hide; // show play area
+			$(".answer_area").show();
+			
+			var correctAns = currentQues.correctans[0];
+
+			currentQues.choices.forEach(function compareAns (ansChoice){
+				if (ansChoice == correctAns) {
+
+				}
+				else {
+
+				}
+
+			} )
+
+		})
+
+
+
+
+	// User clicks for next question
+	var uponNext = $(#nextbtn).click(function nextQues (){
+			$()
+			
+		})
+
+
+
+
+
+	var quesLoop = 
+	displayTheQues.fadeIn();
+	displayAnsChoices.fadeIn();
+	uponSubmit;
+	uponNext;
+		 
+
+		
+		
+		
+
+
 	
-	var correctAns = currentQues.correctans[0];
-	$("#quesForm").append('<li class="ask">' + currentQues.ask + '</li>') // the question. gabe: added quotes around html string
-	$("#quesForm").append('<input id="choices" type="radio">' + possibleAnswers + '<br>')
+
+	//Answer area
+		
+		
+
+	 // gabe: Array of choices for current question. Alternative syntax is: currentQues['choices']
+	
+
+	 // the question. gabe: added quotes around html string
+	
 	// gabe: iterate over each possibleAnswer
 
-	possibleAnswers.forEach(function answeranalysis (possibleAnswer) {
+	// possibleAnswers.forEach(function answeranalysis (possibleAnswer) {
 
-		var value = correctAns == possibleAnswer ? 'right' : 'wrong';f
+		// var value = correctAns == possibleAnswer ? 'right' : 'wrong';
 		 // gabe: added quotes around html string. listed each possibleAns
-	})
+	
 
 })
 	
